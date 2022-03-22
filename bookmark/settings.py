a130be24a59627b22d803ebf4de5630619ef08d1
Rui Liu <forward102910@gmail.com>
2022-03-22 00:59:33 +0000

    enable collect static

--- bookmark/settings.py
+++ bookmark/settings.py
@@ -172,10 +172,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, 'frontend/build/static')
+     os.path.join(BASE_DIR, 'frontend/build')
 ]
 
 # Default primary key field type
